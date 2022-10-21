16b99381a031ee17abd77e9196d3a260dd0b594c
yoosmg <sryu2s@gmail.com>
2022-10-21 16:56:10 +0900

    할 일 삭제하기 개발 완료

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Todo 추가 기능 구현완료
+          Todo 추가 기능, 삭제 기능 구현완료
         </a>
       </header>
     </div>
